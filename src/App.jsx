a6a654c6380407100915f55e3dc7d4cbf44abff3
Eliel-Carvalho <elielcarvalho0605@gmail.com>
2022-09-28 00:35:43 -0300

    Listagem de filmes e séries, estilos css, filme em destaque pendente

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,45 @@
-import React, {useEffect} from "react"
+import React, {useEffect, useState} from "react"
+import FeaturedMovie from "./components/FeaturedMovie"
+import MovieRow from "./components/movieRow"
 import Tmdb from "./tmdb"
+
+import "./app.css"
 
 function App() {
 
+  const [movieList, setMovieList] = useState([])
+  const [featuredData, setFeaturedData] = useState(null)
+
   useEffect(() => {
+    //PEGAR LISTA TOTAL
     const loadAll = async () => {
       let list = await Tmdb.getHomeList()
-      console.log(list)
+      setMovieList(list)
+
+      //PEGAR FILME DESTAQUE
+      let originals = list.filter(i=>i.slug === 'originals')
+      let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1)) //gerando numero aleatorio
+      let chosen = originals[0].items.results[randomChosen]
+
+      console.log(chosen)
     }
 
     loadAll()
   }, [])
-  
+
   return (
-    <div>ola mundo</div>
+    <div className="page">
+      {featuredData && 
+        <FeaturedMovie item={featuredData}/>
+      }
+
+      <section className="lists">
+        {
+          movieList.map((item, key)=>(
+           <MovieRow key={key} title={item.title} items={item.items}/>
+          ))}
+      </section>
+    </div>
   )
 }
 
---
+++ src/components/FeaturedMovie.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./FeaturedMovie.css"
+
+function components() {
+  return(
+    <div>eliel</div>
+  );
+}
+
+export default components;
